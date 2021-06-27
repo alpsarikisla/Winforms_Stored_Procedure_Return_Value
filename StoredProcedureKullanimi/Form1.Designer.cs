52424951e7c9bb73124f01320ab5d563c14792c7
TospaCoder <50963484+alpsarikisla@users.noreply.github.com>
2021-06-27 13:51:47 +0300

    Firt Commit

---
+++ StoredProcedureKullanimi/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace StoredProcedureKullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_isim = new System.Windows.Forms.TextBox();
+            this.tb_aciklama = new System.Windows.Forms.TextBox();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_ekle);
+            this.groupBox1.Controls.Add(this.tb_aciklama);
+            this.groupBox1.Controls.Add(this.tb_isim);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 186);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kategori Ekle";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 234);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategori Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Açıklama:";
+            // 
+            // tb_isim
+            // 
+            this.tb_isim.Location = new System.Drawing.Point(105, 26);
+            this.tb_isim.Name = "tb_isim";
+            this.tb_isim.Size = new System.Drawing.Size(277, 20);
+            this.tb_isim.TabIndex = 2;
+            // 
+            // tb_aciklama
+            // 
+            this.tb_aciklama.Location = new System.Drawing.Point(105, 52);
+            this.tb_aciklama.Multiline = true;
+            this.tb_aciklama.Name = "tb_aciklama";
+            this.tb_aciklama.Size = new System.Drawing.Size(277, 81);
+            this.tb_aciklama.TabIndex = 2;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Location = new System.Drawing.Point(273, 139);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(109, 23);
+            this.btn_ekle.TabIndex = 3;
+            this.btn_ekle.Text = "Kategori Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.TextBox tb_aciklama;
+        private System.Windows.Forms.TextBox tb_isim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ StoredProcedureKullanimi/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoredProcedureKullanimi
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con; SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog=NORTHWND;Integrated Security = true");
+            cmd = con.CreateCommand();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BilgileriDoldur();
+        }
+
+        
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd.CommandText = "usp_YoksaKategoriEkle";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@isim", tb_isim.Text);
+                cmd.Parameters.AddWithValue("@aciklama", tb_aciklama.Text);
+                var returnParameter = cmd.Parameters.Add("@ReturnVal", SqlDbType.Int);
+                returnParameter.Direction = ParameterDirection.ReturnValue;
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                int result = Convert.ToInt32(returnParameter.Value);
+                if (result == -1)
+                {
+                    MessageBox.Show("Kategori Mevcut", "Eklenmedi");
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata");
+            }
+            finally
+            {
+                con.Close();
+            }
+            BilgileriDoldur();
+        }
+
+        private void BilgileriDoldur()
+        {
+            try
+            {
+                cmd.CommandText = "SELECT CategoryID, CategoryName, Description FROM Categories";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Clear();
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+    }
+}
